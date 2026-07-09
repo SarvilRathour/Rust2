ddabc41c23dba677e39a036292695aebb1f3ac86
Sarvil Rathour <ken.kaneki.260905@gmail.com>
2026-07-09 21:03:47 +0530

    feat:added execution of async code in main.rs

--- src/bin/async.rs
+++ src/bin/async.rs
@@ -7,5 +7,12 @@ async fn page_title(url: &str) -> Option<String> {
         .map(|title| title.inner_html())
 }
 fn main(){
-    print!("hello world");
+    let args:Vec<String>=std::env::args().collect();
+    trpl::block_on(async{
+        let url=&args[1];
+        match page_title(url).await{
+            Some(title)=>println!("The title for {url} was {title}"),
+            None=>println!("{url} had no title"),
+        }
+    })
 }
