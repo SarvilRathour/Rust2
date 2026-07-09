ddaef2c524d7bb917db7c6ded48a754710252788
Sarvil Rathour <ken.kaneki.260905@gmail.com>
2026-07-09 22:08:08 +0530

    feat:reading two urls

--- src/bin/async.rs
+++ src/bin/async.rs
@@ -1,18 +1,25 @@
-use trpl::Html;
-async fn page_title(url: &str) -> Option<String> {
+use trpl::{Either,Html};
+async fn page_title(url: &str) -> (&str,Option<String>) {
     let response = trpl::get(url).await;
     let response_text=response.text().await;
-    Html::parse(&response_text)
+    let title=Html::parse(&response_text)
         .select_first("title")
-        .map(|title| title.inner_html())
+        .map(|title| title.inner_html());
+    (url,title)
 }
 fn main(){
-    let args:Vec<String>=std::env::args().collect();
+    let args:Vec<String>  =std::env::args().collect();
     trpl::block_on(async{
-        let url=&args[1];
-        match page_title(url).await{
-            Some(title)=>println!("The title for {url} was {title}"),
-            None=>println!("{url} had no title"),
+        let title_1=page_title(&args[1]);
+        let title_2=page_title(&args[2]);
+        let (url,maybe_title)=match trpl::select(title_1,title_2).await{
+                Either::Left(left)=>left,
+                Either::Right(right)=>right, 
+            };
+        println!("{url} returned first");
+        match maybe_title{
+            Some(title)=>println!("Its page title was: '{title}'"),
+            None=>println!("It had no title"),
         }
     })
 }
